0a33e2fd7ed3e75afb96fb04d34b6053524b67c6
Sobz7 <sobzsa7@gmail.com>
2023-04-08 01:22:18 +0200

    Payment and Rental Factory Classes added.

---
+++ src/main/java/za/ac/cput/factory/PaymentFactory.java
@@ -0,0 +1,22 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Payment;
+import za.ac.cput.util.Helper;
+
+import java.time.LocalDate;
+
+public class PaymentFactory {
+
+    public static Payment createPayment(int paymentId, String paymentType, double paymentAmount, LocalDate paymentDate){
+        // paymentType.isEmpty() || paymentType.equalsIgnoreCase("null")
+        if(Helper.isNullorEmpty(paymentType) || Helper.isNullorEmpty(String.valueOf(paymentDate)))
+            return null;
+
+        return new Payment.Builder().setPaymentId(paymentId)
+                .setPaymentType(paymentType)
+                .setPaymentAmount(paymentAmount)
+                .setPaymentDate(paymentDate)
+                .build();
+    }
+
+}
