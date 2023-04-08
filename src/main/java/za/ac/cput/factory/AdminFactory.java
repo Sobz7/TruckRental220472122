aa189a9a728c90a400ee00c87a9db5d2e44c9e61
Chenique Jacobs <cheniquejacobs@gmail.com>
2023-04-08 16:22:47 +0300

    Added AdminFactory and ReturnTruckFactory classes

---
+++ src/main/java/za/ac/cput/factory/AdminFactory.java
@@ -0,0 +1,20 @@
+//AdminFactory Class
+//Author: Chenique Jacobs (220388172)
+//Date: 07/04/2023
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Admin;
+import za.ac.cput.util.Helper;
+
+public class AdminFactory {
+    public static Admin createAdmin(int adminId, String name, String surname, String email){
+        if(!Helper.isValidEmail(email))
+            return null;
+
+        if(Helper.isNullorEmpty(String.valueOf(adminId))||Helper.isNullorEmpty(name)||Helper.isNullorEmpty(surname))
+            return null;
+
+        return new Admin.Builder().setAdminId(adminId).setName(name).setSurname(surname).setEmail(email).build();
+
+    }
+}
