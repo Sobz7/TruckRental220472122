a49cd8910e0c05d9a222301a351e734d90c17f68
Sobz7 <sobzsa7@gmail.com>
2023-04-09 15:21:03 +0200

    Payment and Rental Repository Classes and Interfaces Added.

---
+++ src/main/java/za/ac/cput/repository/PaymentRepository.java
@@ -0,0 +1,76 @@
+// Payment Repository Class
+// Author: Sobantu Malotana (220472122)
+// Date: 07 April 2023
+
+package za.ac.cput.repository;
+
+import za.ac.cput.domain.Payment;
+import za.ac.cput.domain.Rental;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class PaymentRepository implements IPaymentRepository{
+
+    private static PaymentRepository repository = null;
+    private Set<Payment> paymentDB = null;
+    private PaymentRepository(){
+        paymentDB = new HashSet<Payment>();
+    }
+    public static PaymentRepository getRepository() {
+        if (repository == null) {
+            repository = new PaymentRepository() {
+                @Override
+                public Payment read(String s) {
+                    return null;
+                }
+
+                @Override
+                public boolean delete() {
+                    return false;
+                }
+            };
+        }
+        return repository;
+    }
+    @Override
+    public Payment create(Payment payment) {
+        boolean success = paymentDB.add(payment);
+        if (!success)
+            return null;
+        return payment;
+    }
+    @Override
+    public  Payment read(int paymentId) {
+        for (Payment e : paymentDB) {
+            if(e.getPaymentId().equals(paymentId));
+
+            return e;
+        }
+        return null;
+    }
+
+    @Override
+    public Payment update(Payment payment) {
+        Payment oldPayment =  read((Integer) payment.getPaymentId());
+        if (oldPayment != null) {
+            paymentDB.remove(oldPayment);
+            paymentDB.add(payment);
+            return payment;
+        }
+        return null;
+    }
+    @Override
+    public boolean delete(int paymentId) {
+        Payment paymentToDelete = read(paymentId);
+        if (paymentToDelete == null)
+            return false;
+        paymentDB.remove(paymentToDelete);
+        return true;
+    }
+    @Override
+    public Set<Payment> getAll() {
+        return paymentDB;
+    }
+}
+
