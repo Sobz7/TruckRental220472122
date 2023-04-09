63f1497781ef78e725abaf0b4c4ebf58f7ddcee5
Sobz7 <sobzsa7@gmail.com>
2023-04-09 16:57:45 +0200

    Payment and Rental Factory Test Classes Added.

---
+++ src/test/java/za/ac/cput/factory/RentalFactoryTest.java
@@ -0,0 +1,20 @@
+// RentalFactory Test.java
+// Test for the Rental Factory
+// Author: Sobantu Malotana (220472122)
+// Date: 07 April 2023
+
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Rental;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RentalFactoryTest {
+    @Test
+    public void test(){
+        Rental rental = RentalFactory.createRental(122,12,123,"2023-04-04","2023-05-30",42750.0);
+        System.out.println(rental.toString());
+        assertNotNull(rental);
+    }
+}
