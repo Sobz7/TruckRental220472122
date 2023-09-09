3797720c4237288f2ff70a50201b657b1923f3bd
JordyMeye <80607541+JordyMeye@users.noreply.github.com>
2023-09-09 18:53:56 +0200

    Revert "customer and protection classes added"

--- src/main/java/za/ac/cput/factory/CustomerFactory.java
+++ src/main/java/za/ac/cput/factory/CustomerFactory.java
@@ -3,8 +3,6 @@
 // Author: Jordy Meye (220072841)
 // Date: 24 03 2023
 
-// Customer Factory class
-
 package za.ac.cput.factory;
 
 import za.ac.cput.domain.Customer;
@@ -12,22 +10,16 @@ import za.ac.cput.util.Helper;
 
 public class CustomerFactory {
 
-    public static Customer createCustomer(String name, String surname, String email, String address, String licenseNumber) {
-        String customerId = Helper.generateId();
+    public static Customer createEmployee(String customerId, String name, String surname , String email , String address, String licenseNumber){
 
-        if (!Helper.isValidEmail(email) || Helper.isNullorEmpty(customerId) || Helper.isNullorEmpty(name) ||
-                Helper.isNullorEmpty(address) || Helper.isNullorEmpty(licenseNumber)) {
+        if(!Helper.isValidEmail(email))
+
             return null;
-        }
 
-        return new Customer.Builder()
-                .setCustomerId(customerId)
-                .setName(name)
-                .setSurname(surname)
-                .setEmail(email)
-                .setAddress(address)
-                .setLicenseNumber(licenseNumber)
-                .build();
+        if(Helper.isNullorEmpty(customerId)|| Helper.isNullorEmpty(name) || Helper.isNullorEmpty(address) || Helper.isNullorEmpty(licenseNumber))
+            return null;
+
+        return new Customer.Builder().setCustomerId(licenseNumber).setName(name).setSurname(surname).setEmail(email).setAddress(address).setLicenseNumber(licenseNumber).build();
+
     }
 }
-
